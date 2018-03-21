bfb02034a99a73ece2a6c2fbea0490d00ee287f2
SRoller <sroller@justfab.com>
2018-03-21 13:56:47 -0400

    Initial Commit

---
+++ FunctionCommenter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FunctionCommenter
+//
+//  Created by Shawn Roller on 3/21/18.
+//  Copyright © 2018 Shawn Roller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
